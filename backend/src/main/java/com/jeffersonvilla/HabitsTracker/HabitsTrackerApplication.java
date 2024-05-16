fcdbc758f876baa3af7cdf50fbd0f5d504444075
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-05-16 18:41:41 -0500

    Initialize backend

---
+++ backend/src/main/java/com/jeffersonvilla/HabitsTracker/HabitsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jeffersonvilla.HabitsTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitsTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeffersonvilla/HabitsTracker/HabitsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonvilla.HabitsTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
